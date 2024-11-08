ba15cf1f37cc2145c683bafe1792f2230f119276
Michael Schönburg <michaelschoenburg@gmail.com>
2024-11-08 21:53:03 +0100

    clean up

--- Voraussetzungen.ps1
+++ Voraussetzungen.ps1
@@ -4,5 +4,4 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 Install-Module PowerShellGet -Scope AllUsers
 
-# Install-Module -Name Az -Scope AllUsers -Repository PSGallery -Force
 Install-Module -Name Microsoft.Graph -Scope AllUsers -Repository PSGallery -Force
